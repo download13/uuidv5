a49ca58f4899691bbed41f50fcb2f6a3152d05c5
Erin Dachtler <download333@gmail.com>
2014-09-23 15:51:51 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var uuidv5 = require('./uuid');
+
+
+describe('UUIDv5', function() {
+	it('makes DNS uuids', function() {
+		var r = uuidv5('dns', 'www.example.org');
+
+		assert.equal(r, '74738ff5-5367-5958-9aee-98fffdcd1876');
+	});
+
+	it('makes URL uuids', function() {
+		var r = uuidv5('url', 'http://example.org/page');
+
+		assert.equal(r, '6b19973b-8154-5782-bca0-15e6b730ca00');
+	});
+
+	it('makes OID uuids', function() {
+		var r = uuidv5('oid', '1.3.6.1.2.1');
+
+		assert.equal(r, '2a7086de-bbf9-5cb9-9a66-a9e928c26504');
+	});
+
+	it('makes X.500 uuids', function() {
+		var r = uuidv5('oid', 'uid=user,ou=people,dc=example,dc=com');
+
+		assert.equal(r, '075749ab-61eb-54ec-807d-863ebd6f271a');
+	});
+});
---
+++ uuid.js
@@ -0,0 +1,118 @@
+var crypto = require('crypto');
+
+
+var NAMESPACE_DNS = uuidFromString('6ba7b810-9dad-11d1-80b4-00c04fd430c8');
+var NAMESPACE_URL = uuidFromString('6ba7b811-9dad-11d1-80b4-00c04fd430c8');
+var NAMESPACE_OID = uuidFromString('6ba7b812-9dad-11d1-80b4-00c04fd430c8');
+var NAMESPACE_X500 = uuidFromString('6ba7b814-9dad-11d1-80b4-00c04fd430c8');
+var NAMESPACE_NULL = uuidFromString('00000000-0000-0000-0000-000000000000');
+
+// TODO: Add named default namespaces
+function createUUIDv5Binary(namespace, name) {
+	var c = Buffer.concat([namespace, name], namespace.length + name.length);
+
+	var digest = crypto.createHash('sha1').update(c).digest();
+	var uuid = new Buffer(16);
+
+	// bbbb - bb - bb - bb - bbbbbb
+	digest.copy(uuid, 0, 0, 4); // time_low
+	digest.copy(uuid, 4, 4, 6); // time_mid
+	digest.copy(uuid, 6, 6, 8); // time_hi_and_version
+	uuid[6] = (uuid[6] & 0x0f) | 0x50; // version, 4 most significant bits are set to version 5 (0101)
+	uuid[8] = (digest[8] & 0x3f) | 0x80; // clock_seq_hi_and_reserved, 2msb are set to 10
+	uuid[9] = digest[9];
+	digest.copy(uuid, 10, 10, 16);
+
+	return uuid;
+}
+
+function uuidToString(uuid) {
+	if(!Buffer.isBuffer(uuid)) {
+		throw new Error('uuid must be a Buffer');
+	}
+
+	if(uuid.length !== 16) {
+		throw new Error('uuid buffer length must be 16');
+	}
+
+	var raw = '';
+
+	for(var i = 0; i < 16; i++) {
+		var n = uuid[i].toString(16);
+		if(n.length < 2) n = '0' + n;
+		raw += n;
+	}
+
+	var r = raw.substr(0, 8)
+		+ '-' + raw.substr(8, 4)
+		+ '-' + raw.substr(12, 4)
+		+ '-' + raw.substr(16, 4)
+		+ '-' + raw.substr(20);
+
+	return r.toLowerCase();
+}
+
+function uuidFromString(uuid) {
+	if(typeof uuid !== 'string') {
+		throw new Error('uuid must be a string');
+	}
+
+	var raw = uuid.replace(/-/g, '');
+	if(raw.length !== 32) {
+		throw new Error('uuid string length must be 32 with -\'s removed');
+	}
+
+	var octets = [];
+
+	for(var i = 0; i < 16; i++) {
+		octets[i] = parseInt(raw.substr(i * 2, 2), 16);
+	}
+
+	return new Buffer(octets);
+}
+
+function createUUIDv5(namespace, name, binary) {
+	if(!Buffer.isBuffer(namespace)) {
+		switch(namespace) { // Default namespaces
+		case 'dns':
+			namespace = NAMESPACE_DNS;
+			break;
+
+		case 'url':
+			namespace = NAMESPACE_URL;
+			break;
+
+		case 'oid':
+			namespace = NAMESPACE_OID;
+			break;
+
+		case 'x500':
+			namespace = NAMESPACE_X500;
+			break;
+
+		case 'null':
+			namespace = NAMESPACE_NULL;
+			break;
+
+		default:
+			namespace = uuidFromString(namespace);
+		}
+	}
+
+	if(!Buffer.isBuffer(name)) {
+		name = new Buffer(name, 'utf8');
+	}
+
+	var uuid = createUUIDv5Binary(namespace, name);
+	if(!binary) {
+		uuid = uuidToString(uuid);
+	}
+
+	return uuid;
+}
+
+createUUIDv5.uuidToString = uuidToString;
+createUUIDv5.uuidFromString = uuidFromString;
+
+
+module.exports = createUUIDv5;
